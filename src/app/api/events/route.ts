9a787706b7de07b66866134babdf3d171e5693f7
Santi Iraola <iraola.santi@gmail.com>
2024-05-14 12:32:41 -0400

    force-dynamic

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,29 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { onStatusChange } from '../../lib/statusEmitter';
 
-export const runtime = 'edge';
+export const dynamic = 'force-dynamic'
 
 export function GET(req: NextRequest) {
-  return new Response(
-    new ReadableStream({
-      start(controller) {
-        const onStatusChangeCallback = (status: any) => {
-          controller.enqueue(`data: ${JSON.stringify(status)}\n\n`);
-        };
+  const stream = new ReadableStream({
+    start(controller) {
+      const onStatusChangeCallback = (status: any) => {
+        controller.enqueue(`data: ${JSON.stringify(status)}\n\n`);
+      };
 
-        onStatusChange(onStatusChangeCallback);
+      onStatusChange(onStatusChangeCallback);
 
-        req.signal.addEventListener('abort', () => {
-          controller.close();
-        });
-      }
-    }),
-    {
-      headers: {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache, no-transform',
-        'Connection': 'keep-alive',
-      }
+      req.signal.addEventListener('abort', () => {
+        controller.close();
+      });
     }
-  );
+  });
+
+  return new NextResponse(stream, {
+    headers: {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache, no-transform',
+      'Connection': 'keep-alive',
+    }
+  });
 }
